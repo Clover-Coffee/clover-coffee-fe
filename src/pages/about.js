4d970f16e40173f8876d5e8de90bda093b83587d
Nathan Cai <nathan97cai@gmail.com>
2023-04-06 12:27:58 -0400

    added layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,21 @@
 import React from "react";
+import Layout from "@/components/Layout";
 
 const About = () => {
   return (
-    <div className="wrapper">
-      <div className="mission-statement">
-        <h1>add image</h1>
+    <Layout>
+      <div className="wrapper">
+        <div className="mission-statement">
+          <h1>add image</h1>
+        </div>
+        <div className="core-values">
+          <p className="core-p">values</p>
+        </div>
+        <div className="history">
+          <p className="history-values">history</p>
+        </div>
       </div>
-      <div className="core-values">
-        <p className="core-p">
-            values
-        </p>
-      </div>
-      <div className="history">
-        <p className="history-values">
-            history
-        </p>
-      </div>
-    </div>
+    </Layout>
   );
 };
 
