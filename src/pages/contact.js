0289eb432391186fc4079d48955d1a6581d12515
Nathan Cai <nathan97cai@gmail.com>
2023-04-06 22:23:48 -0400

    added back contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "../styles/contact.module.css";
+import Layout from "@/components/Layout";
+
+const Form = () => {
+  return (
+    <Layout>
+      <div className={`${styles.contactForm} ${styles.greenText}`}>
+        <form className={styles.mainForm}>
+          <label>YOUR NAME</label>
+          <input
+            className={styles.contactInputs}
+            type="text"
+            name="name"
+            placeholder="Name"
+          ></input>
+          <label>EMAIL</label>
+          <input
+            className={styles.contactInputs}
+            type="text"
+            name="name"
+            placeholder="Email"
+          ></input>
+          <label>MESSAGE</label>
+          <textarea
+            className={styles.textArea}
+            rows="6"
+            placeholder="Type your message here"
+          ></textarea>
+          <button className="btn" type="submit">
+            SEND
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Form;
