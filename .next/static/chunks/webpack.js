03cd67ff34dab0519375941ace383e862bb03139
Sonam Tsering <sonam.tsering@sonam.tsering-MacBook-Pro>
2023-04-10 21:39:37 -0400

    Single coffee page responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5ac95534c3c04741"; }
+/******/ 		__webpack_require__.h = function() { return "16a4e8398be5311d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
