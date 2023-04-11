573ab4a939e829f2d985c08ed8dcb1a6c36b3887
Sonam Tsering <sonam.tsering@sonam.tsering-MacBook-Pro>
2023-04-11 14:20:07 -0400

    carousel responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c100db8099bf16b1"; }
+/******/ 		__webpack_require__.h = function() { return "04fb2091b61cde4c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
