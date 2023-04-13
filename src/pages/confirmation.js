5bc130affc09d653db4d7dacaebd186453c9c9cb
Nathan Cai <nathan97cai@gmail.com>
2023-04-13 00:19:20 -0400

    not working yet:

--- src/pages/confirmation.js
+++ src/pages/confirmation.js
@@ -9,6 +9,7 @@ const Confirmation = () => {
   const router = useRouter();
   const orderTotal = router.query.orderTotal;
   const cloverFees = router.query.cloverFees;
+  const totalCloverFees = router.query.totalCloverFees;
 
   return (
     <Layout>
@@ -16,8 +17,9 @@ const Confirmation = () => {
         <h1 className={styles.thankYou}>Thank you!</h1>
         <p className={styles.confirmationText}>Your Order Number is #{orderNumber}</p>
         <p className={styles.community}>We appreciate your support for our business and choosing to shop small with us. Your purchase helps support our team and our community, and it wouldn't be possible without the support of customers like you.</p>
-        {/* <p className={styles.confirmationText}>Order Total: ${orderTotal}</p>
-        <p className={styles.confirmationText}>Clover Fees: ${cloverFees}</p> */}
+        {/* <p className={styles.confirmationText}>Order Total: ${orderTotal}</p> */}
+        <p className={styles.confirmationText}>Clover Fees: ${cloverFees}</p>
+        <p className={styles.confirmationText}>Total Clover Fees: ${totalCloverFees}</p>
       </div>
     </Layout>
   );
